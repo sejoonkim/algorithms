48c647ea8564c83912e38221277303b348687968
sejoonkim <sejoondev@gmail.com>
2020-05-04 23:53:26 +0900

    Added: dp - Practice

---
+++ .study/2020 intern practice/dp/practice.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n;
+int d[1000005];
+
+int main(void) {
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);
+
+    cin >> n;
+
+    d[1] = 0;
+
+    for (int i = 2; i <= n; i++) {
+        d[i] = d[i-1] + 1;
+        if (i % 2 == 0) d[i] = min(d[i], d[i/2] + 1);
+        if (i % 3 == 0) d[i] = min(d[i], d[i/3] + 1);
+    }
+
+    cout << d[n] << '\n';
+    return 0;
+}
