6494ab0a5518f03029d33bc6f3ba3f2237905977
sejoonkim <sejoondev@gmail.com>
2020-09-03 08:42:29 +0900

    fix: inflearn 66

--- .study/inflearn/66.dfs_path_exploration_adjlist.cpp
+++ .study/inflearn/66.dfs_path_exploration_adjlist.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 int ch[30], cnt = 0, n;
+
+// declare the vector
 vector<int> map[30];
 
 void DFS(int v) {
@@ -11,6 +13,7 @@ void DFS(int v) {
         cnt++;
     }
     else {
+        // loop for elements in map[v]
         for (i = 0; i < map[v].size(); i++) {
             if (ch[map[v][i]] == 0) {
                 ch[map[v][i]] = 1;
@@ -30,6 +33,7 @@ int main() {
     cin >> n >> m;
     for (i = 1; i <= m; i++) {
         cin >> a >> b;
+        // construct linked list
         map[a].push_back(b);
     }
     ch[1] = 1;
