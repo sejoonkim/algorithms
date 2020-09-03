7f702980947f9d48ba027a27e793640ecae69e82
sejoonkim <sejoondev@gmail.com>
2020-09-04 08:47:19 +0900

    fix: Inflearn 75 - max income schedule

--- .study/inflearn/75.largest_income_schedule_pqueue.cpp
+++ .study/inflearn/75.largest_income_schedule_pqueue.cpp
@@ -48,56 +48,116 @@ using namespace std;
 // }
 
 // sensei
+// struct Data {
+//     int money;
+//     int when;
+    
+//     // constructor
+//     Data (int a, int b) {
+//         money = a;
+//         when = b;
+//     }
+
+//     bool operator<(Data &b) {
+//         return when > b.when;
+//     } // 날짜에 의해서 내림차순 정렬
+// };
+
+// int main() {
+//     int n, i, j, a, b, res = 0, max = -2147000000;
+
+//     // vector of Data structs
+//     vector<Data> T;
+
+//     // save only money data
+//     priority_queue<int> pQ;
+//     cin >> n;
+
+//     // get money and when
+//     // call constructor
+//     for (i = 1; i <= n; i++) {
+//         cin >> a >> b;
+//         T.push_back(Data(a, b));
+//         if (b > max) {
+//             max = b;
+//         }
+//     }
+//     sort(T.begin(), T.end());
+//     j = 0;
+//     // date limits from 3, 2, 1
+//     for (i = max; i >= 1; i--) {
+//         for ( ; j < n; j++) {
+//             // when date limit is smaller than i break;
+//             if (T[j].when < i) break;
+//             pQ.push(T[j].money);
+//         }
+//         if (!pQ.empty()) {
+//             res += pQ.top();
+//             pQ.pop();
+//         }
+//     }
+
+//     cout << res;
+//     return 0;
+// }
+
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
 struct Data {
     int money;
     int when;
-    
-    // constructor
     Data (int a, int b) {
         money = a;
         when = b;
     }
-
     bool operator<(Data &b) {
         return when > b.when;
-    } // 날짜에 의해서 내림차순 정렬
+    }
 };
 
-int main() {
-    int n, i, j, a, b, res = 0, max = -2147000000;
+int main(void) {
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);
 
-    // vector of Data structs
-    vector<Data> T;
-
-    // save only money data
-    priority_queue<int> pQ;
+    int n, i, j, a, b, maxVal = -2147000000, sum = 0;
     cin >> n;
 
-    // get money and when
-    // call constructor
-    for (i = 1; i <= n; i++) {
+    vector<Data> T;
+    priority_queue<int> pQ;
+
+    for (i = 0; i < n; i++) {
         cin >> a >> b;
         T.push_back(Data(a, b));
-        if (b > max) {
-            max = b;
+        if (b > maxVal) {
+            maxVal = b;
         }
     }
+
     sort(T.begin(), T.end());
+
+    for (i = 0; i < n; i++) {
+        cout << T[i].money << " : " << T[i].when << '\n';
+    }
+
     j = 0;
-    // date limits from 3, 2, 1
-    for (i = max; i >= 1; i--) {
-        for ( ; j < n; j++) {
-            // when date limit is smaller than i break;
-            if (T[j].when < i) break;
-            pQ.push(T[j].money);
+    for (i = maxVal; i >= 1; i--) {
+        for (; j < n; j++) {
+            if (T[j].when < i) break; 
+                pQ.push(T[j].money);
+            
         }
         if (!pQ.empty()) {
-            res += pQ.top();
+            sum += pQ.top();
             pQ.pop();
         }
+        
     }
 
-    cout << res;
+    cout << sum;
+
     return 0;
 }
-
